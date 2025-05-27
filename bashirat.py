aba7a13d19df170a017ca2004cab93dade543045
BashiratMusa <bashmusah2014@gmail.com>
2025-05-27 06:39:01 -0700

    reversed function

---
+++ bashirat.py
@@ -0,0 +1,73 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "5294bd1c-d336-4c79-bad5-95b1c6fea10c",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "def reverse ():\n",
+    "    x=input('enter a sentence')\n",
+    "    return (x[::-1])"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "id": "21bd8013-f000-4989-825d-343c240f1d20",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "enter a sentence i am a boy\n"
+     ]
+    },
+    {
+     "data": {
+      "text/plain": [
+       "'yob a ma i'"
+      ]
+     },
+     "execution_count": 3,
+     "metadata": {},
+     "output_type": "execute_result"
+    }
+   ],
+   "source": [
+    "reverse()"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "2ac453bd-265f-429a-bde2-d72a963db007",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.13.2"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
